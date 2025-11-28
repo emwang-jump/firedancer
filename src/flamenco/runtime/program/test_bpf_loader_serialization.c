c9aec5e7127ef162b1f292bc800b3c3f644cf0d1
Tom Pointon <topointon@jumptrading.com>
2025-11-28 16:40:23 +0000

    fixup

--- src/flamenco/runtime/program/test_bpf_loader_serialization.c
+++ src/flamenco/runtime/program/test_bpf_loader_serialization.c
@@ -604,7 +604,7 @@ main( int argc, char ** argv ) {
   if( cpu_idx>fd_shmem_cpu_cnt() ) cpu_idx = 0UL;
 
   char const * _page_sz = fd_env_strip_cmdline_cstr ( &argc, &argv, "--page-sz",  NULL, "normal" );
-  ulong        page_cnt = fd_env_strip_cmdline_ulong( &argc, &argv, "--page-cnt", NULL, 524288UL ); /* 2GB workspace */
+  ulong        page_cnt = fd_env_strip_cmdline_ulong( &argc, &argv, "--page-cnt", NULL, 65536UL ); /* 256MB workspace */
   ulong        numa_idx = fd_env_strip_cmdline_ulong( &argc, &argv, "--numa-idx", NULL, fd_shmem_numa_idx( cpu_idx ) );
 
   ulong page_sz = fd_cstr_to_shmem_page_sz( _page_sz );
